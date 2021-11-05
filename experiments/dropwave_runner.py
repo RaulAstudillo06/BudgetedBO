a784c67a8e04caf1c23f7a45cae1c2d031ac65fb
RaulAstudillo06 <astudillo0606@gmail.com>
2021-11-04 21:31:34 -0400

    Update.

--- experiments/dropwave_runner.py
+++ experiments/dropwave_runner.py
@@ -12,10 +12,9 @@ torch.set_default_dtype(torch.float64)
 debug._set_state(True)
 
 script_dir = os.path.dirname(os.path.realpath(sys.argv[0]))
-sys.path.append(script_dir[:-11] + "budgeted_bo")
-sys.path.append(script_dir[:-11] + "budgeted_bo/acquisition_functions")
+sys.path.append(script_dir[:-12])
 
-from experiment_manager import experiment_manager
+from budgeted_bo.experiment_manager import experiment_manager
 from parametric_cost_function import get_cost_function_parameters
 
 
@@ -41,9 +40,10 @@ def get_objective_cost_function(seed: int) -> Callable:
 
 # Algos
 algo = "EI-PUC-CC"
-algo_params = {"lookahead_n_fantasies": [1, 1, 1], "refill_until_lower_bound_is_reached": True, "soft_plus_transform_budget": False}
-#algos = ["EI"]
-#algos_params = [{}]
+if algo == "B-MS-EI":
+    algo_params = {"lookahead_n_fantasies": [1, 1, 1], "refill_until_lower_bound_is_reached": True}
+else:
+    algo_params = {}
 
 # Run experiment
 if len(sys.argv) == 3:
